5bb2d3b7b29f58842d901143e545ba7beca72458
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2023-12-12 19:10:22 -0700

    feature: implimented wepack and babel

---
+++ client/src/pages/index.js
@@ -0,0 +1,24 @@
+function loadData() {
+    const selectedValue = document.getElementById("itemSelect").value;
+    const apiUrl = `https://jsonplaceholder.typicode.com/${selectedValue}`;
+  
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => displayData(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }
+  
+  function displayData(data) {
+    const dataList = document.getElementById("dataList");
+    dataList.innerHTML = ""; // Clear previous data
+  
+    data.forEach((item) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = JSON.stringify(item);
+      dataList.appendChild(listItem);
+    });
+  }
+  
+  // Initial data load
+  // loadData();
+  
