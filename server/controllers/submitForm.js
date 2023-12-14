077ce4f8accd740e0edac87e7d63d2f04f6bd996
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2023-12-13 18:18:48 -0700

    Introduce 'utils' folder; add 'pythonRunner.js' with placeholder code for future implementation.

--- server/controllers/submitForm.js
+++ server/controllers/submitForm.js
@@ -1,4 +1,8 @@
 // controller for form
 module.exports.submitFormController = (req, res) => {
+    // get data from req
+    // feed data to scripts
+    // send responce success
     
+    // if error send error message
 };
