79f2270fc1beab95e8b98b5787d7c6af2d2ff212
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2023-12-13 18:28:21 -0700

    Installed 'helmet' package and implemented it for enhanced security measures.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const helmet = require('helmet');
 
 const app = express()
 
 const { PORT = 3000 } = process.env
+
+// security measure
+app.use(helmet())
 
 app.listen(() => {
     console.log(`Listening to port: ${PORT}`)
