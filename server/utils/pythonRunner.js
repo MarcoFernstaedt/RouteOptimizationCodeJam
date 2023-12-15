eb48cfa13109c6cb1cdca4ce5c4df7f1fc2d2a14
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2023-12-15 09:55:39 -0700

    wip: run python scripts in expres

--- server/utils/pythonRunner.js
+++ server/utils/pythonRunner.js
@@ -1,10 +1,31 @@
 const { spawn } = require('child_process');
+const script = require('../scripts/')
 
 // function to run python scripts
-const pythonRunner = (cities, algo) => {
-    // let dataToSend = { cities: cities, algo: algo}
-
-
-    // await responce
-    // return data
+const pythonRunner = (data) => {
+    return new Promise((resolve, reject) => {
+        const pythonScriptPath = `${script}`;
+        
+        const pythonProcess = spawn('python', [pythonScriptPath]);
+    
+        let dataToSend = '';
+    
+        pythonProcess.stdout.on('data', (data) => {
+          result += data.toString();
+        });
+    
+        pythonProcess.stderr.on('data', (data) => {
+          reject(data.toString());
+        });
+    
+        pythonProcess.on('exit', (code) => {
+          if (code === 0) {
+            resolve(JSON.parse(result));
+          } else {
+            reject(`Python script exited with code ${code}`);
+          }
+        });
+      });
 }
+
+module.exports = pythonRunner;
