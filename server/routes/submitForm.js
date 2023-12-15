24273f34920cee7b2831c5ca1ba62a3cc54495bc
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2023-12-15 13:48:27 -0700

    fix: submitFormController imported correctly

--- server/routes/submitForm.js
+++ server/routes/submitForm.js
@@ -1,6 +1,6 @@
-const router = require('express').Router();
-const submitFormController = require('../controllers/submitForm')
+const router = require("express").Router();
+const { submitFormController } = require("../controllers/submitForm");
 
-router.get('/', submitFormController)
+router.get("/", submitFormController);
 
 module.exports = router;
